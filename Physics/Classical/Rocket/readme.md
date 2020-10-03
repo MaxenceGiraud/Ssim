17aa1800cd339849b5a826b5b01a68edc9e67625
MaxenceGiraud <maxence9giraud@gmail.com>
2020-10-03 22:38:56 +0200

    [Rocket] [Refactor] make the rocket/planet class inherit body class Make them functionnal one with each other

--- Physics/Classical/Rocket/readme.md
+++ Physics/Classical/Rocket/readme.md
@@ -4,7 +4,7 @@ Simulation of a rocket launch
 
 ## TODO
 - [x] Create classes for Rocket and planet
-- [ ] Implement simple case
-- [ ] Enable the possibility of having multiple stages
-- [ ] Interface with N-Body Simulation (Make Rocket inherit of Body)
+- [x] Interface with N-Body Simulation (Make Rocket inherit of Body)
+- [x] Enable the possibility of having multiple stages
+- [ ] Implement simple example
 - [ ] Enable to have to plan of flight
--- Physics/Classical/Rocket/rocket.py
+++ Physics/Classical/Rocket/rocket.py
@@ -1,64 +1,70 @@
 import numpy as np
-from ..NBody.NBody import gravitation_newton
+from ..NBody.NBody import gravitation_newton,acc,Body
 
 
-class Rocket_SingleStage:
-    def __init__(self,mass_empty,specific_impulse,fuel_capacity,fuel_rate):
-        self.specific_impulse = specific_impulse # seconds
-        self.fuel = fuel_capacity # In Kg
-        #self.fuel_capacity = fuel_capacity
-        #self.fuel_density = fuel_density
-        self.fuel_rate = fuel_rate # In Kg/s
-        self.mass_empty = mass_empty # In Kg
-        self.mass_full = self.mass_empty + self.fuel # In Kg
+class Stage:
+    def __init__(self,empty_mass,fuel,thrust,specific_impulse):
+        self.empty_mass = empty_mass
+        self.fuel = fuel
+        self.thrust = thrust
+        self.specific_impulse = specific_impulse
 
-        self.v = 0 # m/s
-        self.pos = 0 # Height in m
+        self.fuel_rate = 0
 
     @property
     def mass(self):
-         # Forbid mass to be lower than rocket with no fuel
-        return max(self.mass_empty,self.mass_empty + self.fuel)
-
-
-    def delta_v(self,g=9.81):
-        return self.specific_impulse*g * np.log(self.mass_empty/self.mass_full)
-
-    def get_acc(self,planet,dt):
-        f = self.get_force(planet,dt)
-        return f/self.mass
-
-    def update_position(self,planet,dt):
-        self.update_velocity(planet,dt)
-        self.pos = self.pos + self.v*dt
-        if self.pos < 0 : # Collision with planet
-            self.pos = 0
-        
-    def update_velocity(self,planet,dt):
-        acc = self.get_acc(planet,dt)
-        self.v = self.v + acc*dt
+        return self.empty_mass + self.fuel
 
     def update_fuel(self,dt):
-        self.fuel = max(self.fuel - self.fuel_rate*dt,0) # Forbid negative fuel
+        self.fuel = max(self.fuel - self.fuel_rate*dt,0)
 
-    def get_force(self,planet,dt):
-        f_planet = -gravitation_newton(planet.mass,self.mass,self.pos+planet.radius)
-        thrust = self.fuel_rate/dt * self.specific_impulse  # TODO check thrust equation
+        if self.fuel == 0: # False if not more fuel in the stage
+            return False
+        else : 
+            return True
 
-        return f_planet + thrust
+class Rocket(Body):
+    def __init__(self,name,mass,position,velocity,stages,direction,plan):
+        assert (np.sqrt(np.sum(np.array(direction)**2)) >= 0.999) and  (np.sqrt(np.sum(np.array(direction)**2)) <= 1.001), "Direction vector should have magnitude 1"
 
-class Planet:
-    def __init__(self,mass,radius):
-        self.mass = mass # In kg
-        self.radius = radius # In m
+        self.name
+        self.stages = stages # List of stages, the first one being the one at the bottom of the rocket
+        self.direction = direction # direction of the rocket (vector with magnitude 1)
+        self.plan = plan # TODO determine plan structure (list of direction/ lambda function ??)
+
+        self.mass_empty = mass
+        self.pos = position
+        self.velovity = velocity
+    
+    @property
+    def mass(self):
+        mass_stages = np.sum([stage.mass for stage in self.stages]) # Combined mass of all stages
+        return  mass_stages + self.mass_empty
+
+    def get_acceleration(self,list_bodies):
+        f_other_objects = self.applied_force(list_bodies)
+
+        # TODO Compute thrust of current stage
+        # TODO if stage fuel empty, detach stage
+        # TODO update fuel
+
+        if len(self.stages) != 0 :
+            thrust = self.stages[0]
+        else :
+            thrust = 0
+
+        f = f_other_objects + thrust
+        return f
+
+    def delta_v(self,g=9.81):
+        return np.sum([stage.specific_impulse for stage in self.stages])*g * np.log(self.mass_empty/self.mass)
 
 
-def simulate_rocket_launch(rocket,planet,t,dt=0.1):
-    rocket_pos = [rocket.pos]
-    i=0
-    while i < (t/dt):
-        rocket.update_fuel(dt)
-        rocket.update_position(planet,dt)
-        rocket_pos.append(rocket.pos)
-        i+=1
-    return rocket_pos
+class Planet(Body):
+    def __init__(self,name,mass,position,velocity,radius):
+        self.name = name
+        self.mass = mass
+        self.radius = radius
+        
+        self.pos = position
+        self.velocity = velocity
