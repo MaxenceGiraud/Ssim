231b4895f07eaca2f2a886b5b515f3b88e748f65
MaxenceGiraud <maxence9giraud@gmail.com>
2020-11-12 17:33:34 +0100

    [Waves] Extend to the possibility of N sources

--- ssim/Physics/Wave/DoubleSlit/doublesplit.py
+++ ssim/Physics/Wave/DoubleSlit/doublesplit.py
@@ -1,16 +1,32 @@
+#%%
 import numpy as np
 import matplotlib.pyplot as plt 
 
 class Source:
-    def __init__(self,position,intensity=4):
+    def __init__(self,position,intensity=4,freq= 30,phase=0):
         self.pos = position
         self.intensity = intensity
-        # self.phase = phase
+        self.freq = freq
+        self.phase = phase
     
     def compute_wave(self,grid):
-        return self.intensity * np.sin(30*((grid[:,0]-self.pos[0])**2+(grid[:,1]-self.pos[1])**2)**0.5 )
+        return self.intensity * np.sin(self.freq*((grid[:,0]-self.pos[0])**2+(grid[:,1]-self.pos[1])**2)**0.5 +self.phase)
 
-def doublesplit(source1=Source((0,-0.5)) ,source2 = Source((0,0.5)),posx=(0,10),posy=(-5,5),precision=50,display = True):
+def waves_interferences(sources = [Source((0,-0.5)) ,Source((0,0.5))],posx=(0,10),posy=(-5,5),precision=150,display = True):
+    ''' Compute the intensity of the resulting wave on the grid and plot it
+
+    Parameters
+    ----------
+    sources : list of Source object,
+            list of all sources emitting waves
+    posx : tuple,
+            x limit of the grid
+    posy : tuple,
+            x limit of the grid
+    precision : int,
+             number of row pixels of the grid
+    display : boolean, 
+    '''
 
     # Grid points
     precisiony = (max(posy)-min(posy))/(max(posx)-min(posx)) * precision
@@ -20,11 +36,12 @@ def doublesplit(source1=Source((0,-0.5)) ,source2 = Source((0,0.5)),posx=(0,10),
     grid = np.concatenate([xx.reshape(-1, 1), yy.reshape(-1, 1)], axis=-1)
     
     # Individual waves
-    wave1 = source1.compute_wave(grid)
-    wave2 = source2.compute_wave(grid)
+    waves = []
+    for source in sources :
+        waves.append(source.compute_wave(grid))
 
     # Wave superposition
-    final_wave = wave1 + wave2
+    final_wave = np.sum(waves,axis=0)
     final_wave_intensity = final_wave **2
 
     if display :
@@ -33,9 +50,10 @@ def doublesplit(source1=Source((0,-0.5)) ,source2 = Source((0,0.5)),posx=(0,10),
         # Wave
         plt.scatter(grid[:,0],grid[:,1],c=final_wave_intensity,cmap=plt.cm.binary)
         # Sources
-        plt.scatter(*source1.pos,c='blue')
-        plt.scatter(*source2.pos,c='blue')
+        for source  in sources :
+            plt.scatter(*source.pos,c='blue')
         plt.show()
     
     else :
         return final_wave_intensity
+# %%
--- ssim/Physics/Wave/DoubleSlit/readme.md
+++ ssim/Physics/Wave/DoubleSlit/readme.md
@@ -1,8 +1,8 @@
-# Double split experiment with classical waves
+# Waves interferences experiment 
 
-Simulate 2 sources of waves and how they interfere with each other.
-This can be used to recreate the Double split experiment.
+Simulate N sources of waves and how they interfere with each other.
+This can be used to recreate the Double split experiment, and does by default (the 2 sources are considered to be the 2 holes)
 
 ## TODO
 - [x] Implement double split experiment
-- [ ] Extend to n sources
+- [x] Extend to n sources
