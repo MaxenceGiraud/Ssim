95c05fad23cbddea0213c506b76f2708c753d2df
MaxenceGiraud <maxence9giraud@gmail.com>
2020-11-13 23:32:21 +0100

    [Koch] Vectorize code of koch snowflake

--- ssim/Math/KochSnowflake/kochsnow.py
+++ ssim/Math/KochSnowflake/kochsnow.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def koch_snowflake(iterations=2,display_iter=False,display=True):
+def koch_snowflake(iterations=4,display_iter=False,display=True):
     # Starting points
     tan_angle = np.tan(np.pi/3)
     points = [[0,0],[3**iterations/2,tan_angle*3**iterations/2],[3**iterations,0],[0,0]]
@@ -41,6 +41,59 @@ def koch_snowflake(iterations=2,display_iter=False,display=True):
             
         new_points.append(points[-1]) # Add last point
         points = new_points
+
+        if display_iter : 
+            draw(points)
+
+    if display and not display_iter:
+        draw(points)
+
+    return points
+
+
+def koch_snowflake_vec(iterations=4,display_iter=False,display=True):
+    ''' Vectorized version of koch snowflake'''
+    # Starting points
+    tan_angle = np.tan(np.pi/3)
+    points = np.array([[0,0],[3**iterations/2,tan_angle*3**iterations/2],[3**iterations,0],[0,0]])
+
+    def draw(points):
+        plt.figure(figsize=(10,10))
+ 
+        plt.plot(points[:,0],points[:,1])
+        plt.axis('square')
+
+    for _ in range(iterations-1):
+        new_points = [] 
+        
+        points_next = np.roll(points,-1,axis=0)[:-1]
+        diff = points_next - points[:-1]
+
+        side_eq = np.sqrt(np.sum(diff**2,axis=1))/3  # Signed len of new triangle side (/3)
+        angle = np.arctan2(diff[:,1],diff[:,0])
+
+        point1 = points[:-1] + diff/3 # First point of new triangle (first third of line)
+
+        point2 = np.zeros(point1.shape)
+        point2[:,0] = point1[:,0] + side_eq * np.cos(angle+np.pi/3)
+        point2[:,1] = point1[:,1] + side_eq * np.sin(angle+np.pi/3)
+        
+        point3 = points[:-1] + 2*diff/3 # First point of new triangle (first third of line)
+
+        # Concatenate the arrays
+        new_points = np.zeros((point1.shape[0]*4+1,2))
+        idx = np.arange(new_points.shape[0])
+        idx_tmp = np.where(idx%4==0)[0]
+        new_points[idx_tmp] = points
+        idx_tmp = idx_tmp[:-1]
+        idx_tmp +=1
+        new_points[idx_tmp] = point1
+        idx_tmp +=1
+        new_points[idx_tmp] = point2
+        idx_tmp +=1
+        new_points[idx_tmp] = point3
+
+        points  = new_points
 
         if display_iter : 
             draw(points)
--- ssim/Math/KochSnowflake/readme.md
+++ ssim/Math/KochSnowflake/readme.md
@@ -1,4 +1,11 @@
 # Koch snowflake
 
+
+## TODO
+- [x] Implement koch snowflake
+- [x] Vectorize the code
+- [ ] Implement Koch antisnowflake
+- [ ] Add zoom possibility / specify region to visualization
+- [ ] 
 ## Links 
 <https://en.wikipedia.org/wiki/Koch_snowflake>
