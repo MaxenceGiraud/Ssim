342d91607dc2a3b95d1f564e786eea611d42a21f
MaxenceGiraud <maxence9giraud@gmail.com>
2021-04-18 12:05:27 +0200

    Add finite well potentials (1D & 2D) in quantum eigenstate project

--- ssim/Physics/Quantum/EigenstatesSolver/potentials.py
+++ ssim/Physics/Quantum/EigenstatesSolver/potentials.py
@@ -6,11 +6,13 @@ def infinite_well_1D(x):
 def infinite_well_2D(x,y):
     return np.zeros((len(x),len(y)))
 
-# def finite_well_1D(x,well_range=(0.25,0.75)):
-#     return 0
+def finite_well_1D(x,well_range=(0.25,0.75)):
+    box= np.where(x>well_range[0],1,0) + np.where(x<well_range[1],1,0)
+    return np.where(box==2,0,1e15)
 
-# def finite_well_2D(x,y,well_range_x=(0.25,0.75),well_range_y=(0.25,0.75)):
-#     return 0
+def finite_well_2D(x,y,well_range_x=(0.25,0.75),well_range_y=(0.25,0.75)):
+    box = np.where(x>well_range_x[0],1,0) + np.where(x<well_range_x[1],1,0) + np.where(y>well_range_y[0],1,0) + np.where(y<well_range_y[1],1,0)
+    return np.where(box==4,0,1e15)
 
 def Gaussian_1D(x,mu=0.5,sigma=0.1):
     return np.exp(-(x-mu)**2/(2*sigma**2))
